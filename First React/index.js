0f9f79235b6bf4b500a4ab299d924875f5629169
KoukeFoxes <koukefoxes@gmail.com>
2022-06-05 13:23:06 +0800

    ul li

--- First React/index.js
+++ First React/index.js
@@ -1,1 +1,7 @@
-ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById('root'));
+ReactDOM.render(
+    <ul>
+        <li>Thing 1</li>
+        <li>Thing 2</li>
+    </ul>, 
+    document.getElementById('root')
+);
