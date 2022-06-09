7b7479f50ff8794bbeaa97b6afcbea2e11b3e073
KoukeFoxes <koukefoxes@gmail.com>
2022-06-09 14:44:25 +0800

    clean up some files

---
+++ 14.build-project/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Main(){
+    return(
+        <main>
+            <h1 className="main--title">What is KoukeFoxes doing</h1>
+            <ul className="main--facts">
+                <li>Learning Algorithm</li>
+                <li>Learning React</li>
+                <li>Learning Artificial Intelligence</li>
+                <li>Learning Visualization</li>
+                <li>...</li>
+            </ul>
+        </main>
+    )
+}
---
+++ 15.digital-business-card/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App(){
+    return (
+        <div>
+            <h1>Digital Business Card</h1>
+        </div>
+    )
+}
